7f6558240002498faf199f5a077d41f29d6ce921
BodaMops25 <79203099+BodaMops25@users.noreply.github.com>
2022-07-12 17:13:55 +0300

    Add files via upload

--- fetchMachineClient.js
+++ fetchMachineClient.js
@@ -1,5 +1,5 @@
 async function FM_makeFetch(url, {method = 'GET', headers, body} = {}) {
-  return fetch('https://fetch-machine.herokuapp.com//fetch_machine/getFetch', {
+  return fetch('https://fetch-machine.herokuapp.com/fetch_machine/getFetch', {
     method: 'POST',
     body: JSON.stringify({
       url: url,
